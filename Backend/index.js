a67d64d008ff717041aed94af6171042c9a97e9e
Swanand Pande <63854392+swan28pande@users.noreply.github.com>
2024-05-23 22:10:41 +0530

    updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,106 +4,69 @@ const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server,{ 
+const io = socketIO(server, { 
   cors: {
     origin: ["http://localhost:5173", "https://shatranj-app.vercel.app/"],
     methods: ["GET", "POST"],
     credentials: true
   }
-}
-
-, );
-// const io = socketIO(server);
+});
 
 let gameno = 0;
 let Rooms = [];
-port = 4000;
+const port = 4000;
 const generateUniqueId = require('./genID.js');
 const { log } = require('console');
 
 const cors = require('cors');
 app.use(cors());
 
-
 app.get('/chess', (req, res) => {
   res.send('Welcome to Shatranj!')
-})
+});
 
 app.get('/chess/create-game', (req, res) => {
   const gameId = generateUniqueId(gameno); // Implement a function to generate a unique game ID
-  gameno+=1;
+  gameno += 1;
   log(`Created game with ID ${gameId}`);
   res.json({ gameId });
 });
 
-io.on("connection", (socket)=> {
-
+io.on('connection', (socket) => {
   socket.on('create-game', (gameid) => {
-
     log(`Player joined game ${gameid}`);
-
-    socket.join(gameid) ;
+    socket.join(gameid);
     io.to(gameid).emit("roomJoined", `You have joined the room: ${gameid}`);
-
-    
-
-
   });
 
   socket.on('join-game', (gameid) => {
-
-    
-    if(io.of("/").adapter.rooms.get(gameid))
-    {
+    if (io.of("/").adapter.rooms.get(gameid)) {
       const room = io.of("/").adapter.rooms.get(gameid);
       log(`Player joined game ${gameid}`);
-
-      socket.join(gameid) ;
+      socket.join(gameid);
       io.to(gameid).emit("roomJoined", `You have joined the room: ${gameid} with ${room.size} members`);
-
+    } else {
+      console.log("room doesn't exist");
     }
-    else
-    {
-      console.log("room doesnt exist");
+  });
+
+  socket.on('return-members', (gameid) => {
+    if (io.of("/").adapter.rooms.get(gameid)) {
+      const room = io.of("/").adapter.rooms.get(gameid);
+      log(`Player joined game ${gameid}`);
+      io.to(gameid).emit("roomJoined", `You have joined the room: ${gameid} with ${room.size} members`);
+    } else {
+      console.log("room doesn't exist");
     }
-  })
-    socket.on('return-members', (gameid) => {
+  });
 
-    
-      if(io.of("/").adapter.rooms.get(gameid))
-      {
-        const room = io.of("/").adapter.rooms.get(gameid);
-        log(`Player joined game ${gameid}`);
-        io.to(gameid).emit("roomJoined", `You have joined the room: ${gameid} with ${room.size} members`);
-  
-      }
-      else
-      {
-        console.log("room doesnt exist");
-      }
-    })
+  socket.on('send-moves', (game_id, image_pos, whitemove, boarddisable) => {
+    console.log("move received by server");
+    socket.broadcast.to(game_id).emit("receive-moves", image_pos, whitemove, boarddisable);
+    console.log("move received by server");
+  });
+});
 
-      socket.on('send-moves', (game_id,image_pos,whitemove,boarddisable) => {
-          console.log("move recieved to server");
-          socket.broadcast.to(game_id).emit("recieve-moves", image_pos,whitemove,boarddisable);
-          console.log("move recieved to server");
-    
-        })
-})
-
-
-  app.get('/chess', (req, res) => {
-    res.send('Welcome to Shatranj!')
-  })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-server.listen(4000);
-
-
- //1. Signup 
- //2. Login 
- //3. Play with Friend 
- //4. Random 
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
